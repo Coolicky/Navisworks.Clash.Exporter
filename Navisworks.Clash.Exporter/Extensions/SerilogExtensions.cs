42a4f8c91f8388d968efd265fe899c602d5d3d2b
Piotr Kulicki <piotr.kulicki@gmail.com>
2022-07-13 09:30:55 +0100

    Serilog Pipe Extension

---
+++ Navisworks.Clash.Exporter/Extensions/SerilogExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Navisworks.Clash.Exporter.Logger;
+using Serilog;
+using Serilog.Configuration;
+
+namespace Navisworks.Clash.Exporter.Extensions
+{
+    public static class SerilogExtensions
+    {
+        public static LoggerConfiguration PipeSink(
+            this LoggerSinkConfiguration loggerConfig,
+            StreamWriter streamWriter,
+            IFormatProvider formatProvider = null)
+        {
+            return loggerConfig.Sink(new PipeSink(formatProvider, streamWriter));
+        }
+    }
+}
