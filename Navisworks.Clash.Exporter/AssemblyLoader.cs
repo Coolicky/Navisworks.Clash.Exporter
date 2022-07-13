7bb34f7938dbe0194924b3ee92a5e5a221e976a1
Piotr Kulicki <piotr.kulicki@gmail.com>
2022-07-13 08:29:49 +0100

    Assembly Loader

---
+++ Navisworks.Clash.Exporter/AssemblyLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace Navisworks.Clash.Exporter
+
+{
+    [Plugin("PM.Navisworks.ClashExporter.Loader", "COOL")]
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class AssemblyLoader : EventWatcherPlugin
+    {
+        private static string _thisAssemblyPath;
+
+        public static Assembly ResolveAssemblies(object sender, ResolveEventArgs args)
+        {
+            var path = Path.GetDirectoryName(_thisAssemblyPath);
+            if (path == null) return null;
+            var dll = $"{new Regex(",.*").Replace(args.Name, string.Empty)}.dll";
+            var file = Path.Combine(path, dll);
+
+            return !File.Exists(file) ? null : Assembly.LoadFrom(file);
+        }
+
+        public override void OnLoaded()
+        {
+            _thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssemblies;
+        }
+
+        public override void OnUnloading()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= ResolveAssemblies;
+        }
+    }
+}
