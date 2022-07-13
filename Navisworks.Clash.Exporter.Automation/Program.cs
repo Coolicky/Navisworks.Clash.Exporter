681feac802df1ee02c5547ef41741f242bb3e1c2
Piotr Kulicki <piotr.kulicki@gmail.com>
2022-07-13 08:17:15 +0100

    Initial Setup

---
+++ Navisworks.Clash.Exporter.Automation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Navisworks.Clash.Exporter.Automation
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Navisworks.Clash.Exporter.Setup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Navisworks.Clash.Exporter.Setup
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
