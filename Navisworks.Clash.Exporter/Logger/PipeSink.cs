d78a287821d7f129194dba2e6e1e39661a89c95f
Piotr Kulicki <piotr.kulicki@gmail.com>
2022-07-13 08:42:53 +0100

    Serilog Setup for Pipe Logging.

---
+++ Navisworks.Clash.Exporter/Logger/PipeSink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Navisworks.Clash.Exporter.Logger
+{
+    public class PipeSink : ILogEventSink
+    {
+        private readonly IFormatProvider _formatProvider;
+        private readonly StreamWriter _writer;
+
+        public PipeSink(IFormatProvider formatProvider, StreamWriter writer)
+        {
+            _formatProvider = formatProvider;
+            _writer = writer;
+        }
+        public void Emit(LogEvent logEvent)
+        {
+            var message = logEvent.RenderMessage(_formatProvider);
+            _writer.WriteLine(message);
+            _writer.Flush();
+        }
+    }
+}
